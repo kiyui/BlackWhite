f4c33514b6e5dc1a45f6c8f0e79a000e9ad525de
TimurKiyivinski <deathseclipse@gmail.com>
2014-04-24 21:33:31 +0800

    MEEEOOOOWWW

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include<stdbool.h>
 #include<string.h>
 #include<curses.h>
-
+#define DIM 8
 struct player
 {
 	char name[32];
@@ -57,7 +57,7 @@ void drawBoard(char board[8][8], struct player player1, struct player player2, c
 	
 	if (!strcmp(*gameMessage, "1"))
 	{
-		printw("Player %s's turn!", player1.name);
+		printw("\nPlayer %s's turn!", player1.name);
 	}
 	else if (!strcmp(*gameMessage, "2"))
 	{
@@ -65,11 +65,11 @@ void drawBoard(char board[8][8], struct player player1, struct player player2, c
 	}
 	else
 	{
-		//printf("\n%s", *gameMessage);
+		printw("\n%s", *gameMessage);
 	}
 	
-	printw("%s\t :\t%i\n", player1.name, player1.score);
-	printw("%s\t :\t%i\n", player2.name, player2.score);
+	printw("\n%s\t :\t%i", player1.name, player1.score);
+	printw("\n%s\t :\t%i\n", player2.name, player2.score);
 	for (loopXVar = 0; loopXVar < 8; loopXVar++)
 	{
 		for (loopYVar = 0; loopYVar < 8; loopYVar++)
@@ -80,6 +80,10 @@ void drawBoard(char board[8][8], struct player player1, struct player player2, c
 		printw("\n");
 	}
 	refresh();
+}
+
+void changeBoard(char board[8][8])
+{
 }
 
 struct player playGame()
@@ -97,18 +101,22 @@ struct player playGame()
 	int playingNo = 1;
 	//Get player scores
 	int score[2][1];
-	//To return the winner
-	struct player winner;
 	//Loops
 	bool gameLoop = true, playLoop = true;
 	int loopXVar, loopYVar;
 	//Players
+	int playerTurn = 2;
 	struct player player1;
 	struct player player2;
+	struct player currentPlayer;
 	player1.token = '@';
         player2.token = 'O';
 	player1.score = 0;
 	player2.score = 0;
+	player1.location[0] = 0;
+	player1.location[1] = 0;
+	player2.location[0] = 0;
+	player2.location[1] = 0;
 	printw("\nPlease input player names:");
 	printw("\nPlayer 1 (@): ");
 	refresh();
@@ -125,40 +133,80 @@ struct player playGame()
 	playBoard[3][3] = player2.token;
 	playBoard[4][4] = player2.token;
 	memcpy(actualBoard, playBoard, sizeof actualBoard);
-	drawBoard(playBoard, player1, player2, &gameMessage);
-	getch();
 	//Initial Display
 	countScore(player1.token, playBoard, &player1.score);
 	countScore(player2.token, playBoard, &player2.score);
-	strcpy(gameMessage, "Welcome sucker");
+	strcpy(gameMessage, "Welcome, players!");
 	drawBoard(playBoard, player1, player2, &gameMessage);
-	getch();
-	/*
+	currentPlayer = player2;
 	while (gameLoop)
 	{
+		if (playerTurn == 2)
+		{
+			strcpy(gameMessage, "1");
+			playerTurn = 1;
+			currentPlayer = player1;
+		}
+		else
+		{
+			strcpy(gameMessage, "2");
+			playerTurn = 2;
+			currentPlayer = player2;
+		}
 		playLoop = true;
 		while (playLoop)
 		{
-			for (loopXVar = 0; loopXVar < 8; loopXVar++)
+			memcpy(playBoard, actualBoard, sizeof playBoard);
+			playBoard[currentPlayer.location[0]][currentPlayer.location[1]] = 'X';
+			drawBoard(playBoard, player1, player2, &gameMessage);
+			playerInput = getch();
+			if (playerInput == 'w' && currentPlayer.location[0] > 0)
 			{
-				for (loopYVar = 0; loopYVar < 8; loopYVar++)
+				currentPlayer.location[0]--;
+			}
+			else if (playerInput == 'a' && currentPlayer.location[1] > 0)
+			{
+				currentPlayer.location[1]--;
+			}
+			else if (playerInput == 'd' && currentPlayer.location[1] < 7)
+			{
+				currentPlayer.location[1]++;
+			}
+			else if (playerInput == 's' && currentPlayer.location[0] < 7)
+			{
+				currentPlayer.location[0]++;
+			}
+			else if (playerInput == 'x')
+			{
+				if (actualBoard[currentPlayer.location[0]][currentPlayer.location[1]] == '@' || actualBoard[currentPlayer.location[0]][currentPlayer.location[1]] == 'O')
 				{
-					if (playBoard[loopXVar][loopYVar] != '@' || playBoard[loopXVar][loopYVar] != 'O' )
-					{
-						playBoard[loopXVar][loopYVar] = 'X';
-					}
+					strcpy(gameMessage, "Cannot set token here!");
+				}
+				else
+				{
+					actualBoard[currentPlayer.location[0]][currentPlayer.location[1]] = currentPlayer.token;
+					countScore(currentPlayer.token, actualBoard, &currentPlayer.score);	
+					playLoop = false;
 				}
 			}
-			if (playerNo == 1)
+			else
 			{
-				strcpy()
+				strcpy(gameMessage, "Invalid move!");
 			}
-			drawBoard(playBoard, score, player1, player2, "")
-
-		}	
-	}*/
-	winner = player1;
-	return winner;
+			switch (playerTurn)
+			{
+				case 1:
+					player1 = currentPlayer;
+					break;
+				case 2:
+					player2 = currentPlayer;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	return currentPlayer;
 }
 
 int main()
